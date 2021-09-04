eb89661342350590c0fbac298770f19f4190c65b
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:21:19 -0400

    kva

--- InterviewCodingTasks/String_RemoveDuplicates.java
+++ InterviewCodingTasks/String_RemoveDuplicates.java
@@ -1,20 +1,30 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 public class String_RemoveDuplicates {
     /*
     Write function that can remove the duplicated values from String
             Ex: removeDup("AAABBBCCC") ==> ABC
      */
 
+    //Solution 1:
     public String removeDup(String str) {
         String result = "";
-
         for (int i = 0; i < str.length(); i++) {
             if (!result.contains("" + str.charAt(i))) {
                 result += "" + str.charAt(i);
             }
         }
-
         return result;
     }
+
+    //Solution 2:
+    public static String removeDup2(String str) {
+        str = new LinkedHashSet<String>(Arrays.asList(str.split(""))).toString();
+        str = str.replace(", " ,  "" ).replace("[","").replace("]","");
+        return  str;
+    }
+
 
 
 }
