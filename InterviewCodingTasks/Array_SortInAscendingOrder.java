9b097703a1d4b61c40443f3af9f0d28b551392b2
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:34:43 -0400

    kva

--- InterviewCodingTasks/Array_SortInAscendingOrder.java
+++ InterviewCodingTasks/Array_SortInAscendingOrder.java
@@ -6,12 +6,11 @@ public class Array_SortInAscendingOrder {
     Write a method that can sort an int array in Ascending order without using the sort method
      */
 
+    //Solution 1:
     public int[] sortingArrayAsc(int[] arr) {
 
         int[] result = Arrays.copyOfRange(arr, 0, arr.length);
-
         for (int i = 0; i < result.length; i++) {
-
             for (int j = 0; j < result.length; j++) {
                 if (result[i] < result[j]) {
                     Integer temp = result[i];
@@ -19,12 +18,10 @@ public class Array_SortInAscendingOrder {
                     result[j] = temp;
                 }
             }
-
         }
-
         return result;
-
     }
+
 
 
 }
