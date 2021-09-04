a0c696fb07b296c0af14fea3dbcdc115f3688944
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:41:01 -0400

    kva

--- InterviewCodingTasks/Array_SumUpTo0.java
+++ InterviewCodingTasks/Array_SumUpTo0.java
@@ -10,17 +10,13 @@ public class Array_SumUpTo0 {
      */
 
     public int[] solution(int N) {
-
         int[] result = new int[N];
         int sum = 0;
-
         for (int i = 0; i < N - 1; i++) {
             result[i] = i;
             sum += i;
         }
-
         result[N - 1] = -sum;
-
         return result;
     }
 
