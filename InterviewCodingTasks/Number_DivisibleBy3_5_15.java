4aed9152666cf60bba981bcdc5cdba05617abca6
yourMD12 <yourmd12@gmail.com>
2021-09-04 19:31:32 -0400

    kva

--- InterviewCodingTasks/Number_DivisibleBy3_5_15.java
+++ InterviewCodingTasks/Number_DivisibleBy3_5_15.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number_DivisibleBy3_5_15 {
 
     /*
@@ -18,41 +21,56 @@ public class Number_DivisibleBy3_5_15 {
                  Divisible By 15 15 30 45 60 75 90
                  Divisible By 5 5 10 20 25 35 40 50 55 65 70 80 85 95 100
                  Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
-
     */
 
-
+    //solution 1:
     public void solution(int n) {
         String divisibleBy15 = "";
         String divisibleBy5 = "";
         String divisibleBy3 = "";
 
         int[] arr = new int[n];
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
-
         for (int each : arr) {
-
             if (each % 15 == 0 && each % 3 == 0) {
                 divisibleBy15 += each + " ";
             }
-
             if (each % 5 == 0 && each % 15 != 0) {
                 divisibleBy5 += each + " ";
             }
-
             if (each % 3 == 0 && each % 15 != 0) {
                 divisibleBy3 += each + " ";
             }
-
         }
 
         System.out.println("Divisible By 15: " + divisibleBy15);
         System.out.println("Divisible By 5: " + divisibleBy5);
         System.out.println("Divisible By 3: " + divisibleBy3);
     }
+
+    //solution 2:
+    public void solution2(int n){
+        String divisibleBy15 ="";
+        String divisibleBy5 ="";
+        String divisibleBy3="";
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i < 100; i++)
+            list.add(i+1);
+        for(int each: list) {
+            if(each %15==0 && each %3==0)
+                divisibleBy15+= each+" ";
+            if(each %5==0 && each % 15!=0)
+                divisibleBy5 += each+" ";
+            if(each%3==0 && each %15!=0)
+                divisibleBy3 += each+" ";
+        }
+        System.out.println("Divisible By 15: "+divisibleBy15);
+        System.out.println("Divisible By 5: "+divisibleBy5);
+        System.out.println("Divisible By 3: "+divisibleBy3);
+    }
+
 
 
 }
