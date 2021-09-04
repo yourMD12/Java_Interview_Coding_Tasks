c0a642715fd58da73fa8313769e5c46252073c28
yourMD12 <yourmd12@gmail.com>
2021-09-04 19:49:55 -0400

    kva

--- InterviewCodingTasks/Map_FrequencyOfCharacters.java
+++ InterviewCodingTasks/Map_FrequencyOfCharacters.java
@@ -10,19 +10,14 @@ public class Map_FrequencyOfCharacters {
     public static void FrequencyTest(String  str ) {
 
         Map<Character, Integer> map = new LinkedHashMap<>();
-
         for (Character each : str.toCharArray()) {
-
             if (map.containsKey(each)) {
                 map.put(each, map.get(each) + 1);
             } else {
                 map.put(each, 1);
             }
-
         }
-
         System.out.println(map);
-
     }
 
 
