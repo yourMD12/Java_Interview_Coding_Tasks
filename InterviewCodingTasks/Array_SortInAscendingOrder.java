be483559fad0d563c73c96d969eae9b96ca9fb8d
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:35:43 -0400

    kva

--- InterviewCodingTasks/Array_SortInAscendingOrder.java
+++ InterviewCodingTasks/Array_SortInAscendingOrder.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Array_SortInAscendingOrder {
@@ -20,6 +21,24 @@ public class Array_SortInAscendingOrder {
             }
         }
         return result;
+    }
+
+    //Solution 2:
+    public static int[] Sort(int[] a) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int each: a)
+            list.add(each);
+        for(int i=0; i < a.length; i++) {
+            a[i] = findMin(list);
+            list.remove(Integer.valueOf(a[i]));
+        }
+        return a;
+    }
+    public static int findMin(ArrayList<Integer> a) {
+        int min =Integer.MAX_VALUE;
+        for(int each: a)
+            min = Math.min(min, each);
+        return min;
     }
 
 
