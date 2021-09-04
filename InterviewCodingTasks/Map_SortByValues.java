abe4e811e486a55ec2a89de2be0d69db8bf0df22
yourMD12 <yourmd12@gmail.com>
2021-09-04 19:48:36 -0400

    kva

--- InterviewCodingTasks/Map_SortByValues.java
+++ InterviewCodingTasks/Map_SortByValues.java
@@ -12,15 +12,11 @@ public class Map_SortByValues {
     public Map<String, Integer> sortByValue(Map<String, Integer> map) {
         List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
         list.sort(Map.Entry.comparingByValue());
-
         map = new LinkedHashMap<>();
-
         for (Map.Entry<String, Integer> each : list) {
             map.put(each.getKey(), each.getValue());
         }
-
         return map;
-
     }
 
 }
