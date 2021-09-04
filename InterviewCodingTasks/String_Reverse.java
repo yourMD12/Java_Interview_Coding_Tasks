d9d110b592aaf1c30a05220a36cd921a2b72d446
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:24:14 -0400

    kva

--- InterviewCodingTasks/String_Reverse.java
+++ InterviewCodingTasks/String_Reverse.java
@@ -5,16 +5,17 @@ public class String_Reverse {
         Ex: Reverse("ABCD"); ==> DCBA
      */
 
+    //Solution 1:
     public String StrReverse(String str) {
 
         String reverse="";
-
         for(int i=str.length()-1; i >= 0; i--) {
             reverse += str.toCharArray()[i];
         }
         return reverse;
-
     }
+
+
 
 
 }
