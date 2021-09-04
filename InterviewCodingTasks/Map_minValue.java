54da25d8514530b0ab7b890a2cb3b8e57f5c75e1
yourMD12 <yourmd12@gmail.com>
2021-09-04 19:51:01 -0400

    kva

---
+++ InterviewCodingTasks/Map_minValue.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Map_minValue {
+
+    //Write a method that can return the minimum value from ta map (DO NOT use sort method)
+
+    public  static  int  minValue( Map<String,Integer> map  ) {
+            SortedSet<Integer> sm = new TreeSet<>(map.values());
+            return sm.first( );
+    }
+}
