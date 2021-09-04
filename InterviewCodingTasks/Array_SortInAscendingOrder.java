9402f9164834ea3f466a84a7cce2f7066e88f7c4
yourMD12 <yourmd12@gmail.com>
2021-09-03 23:36:10 -0400

    kva

--- InterviewCodingTasks/Array_SortInAscendingOrder.java
+++ InterviewCodingTasks/Array_SortInAscendingOrder.java
@@ -41,6 +41,26 @@ public class Array_SortInAscendingOrder {
         return min;
     }
 
+    //Solution 3:
+    public static void SortingArrayAsc(int[] arr) {
+        ArrayList<Integer> list = new ArrayList();
+        for(int each: arr) {
+            list.add(each);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i) < list.get(j)) {
+                    Integer temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        for(int i=0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+    }
+
 
 
 }
