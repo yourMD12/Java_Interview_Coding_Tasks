2fbf0c17740f497647dfa4828f75fdb362b1af2a
yourMD12 <yourmd12@gmail.com>
2021-09-07 00:04:49 -0400

    kva

--- InterviewCodingTasks/String_LongestPalindrome.java
+++ InterviewCodingTasks/String_LongestPalindrome.java
@@ -13,26 +13,20 @@ public class String_LongestPalindrome {
         ArrayList<String> list = new ArrayList<>();
 
         for (int i = 0; i < str.length(); i++) {
-
             for (int j = i + 1; j < str.length(); j++) {
                 list.add(str.substring(i, j));
             }
-
         }
 
         for (String each : list) {
-
             String reverse = "";
             for (int i = each.length() - 1; i >= 0; i--) {
                 reverse += each.charAt(i);
             }
-
             if (each.equalsIgnoreCase(reverse) && each.length() > longest.length()) {
                 longest = each;
             }
-
         }
-
         return longest;
     }
 
