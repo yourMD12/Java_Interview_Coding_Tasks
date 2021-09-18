f8326975ec842fecc2d01213a4661dfd52007ff8
yourMD12 <yourmd12@gmail.com>
2021-09-18 12:45:20 -0400

    kva

--- InterviewCodingTasks/Array_MoveAllZerosToTheEnd.java
+++ InterviewCodingTasks/Array_MoveAllZerosToTheEnd.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Array_MoveAllZerosToTheEnd {
 
     /*
@@ -8,15 +10,25 @@ public class Array_MoveAllZerosToTheEnd {
 
         int[] result = new int[array.length];
         int count = 0;
-
         for (int each : array) {
             if (each != 0)
                 result[count++] = each;
         }
-
         return result;
-
     }
 
+    public static void main(String[] args) {
+        int [] nums = {1,2,0,3,4,0,0,5,6};
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+    }
 
-}
