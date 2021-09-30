b88b87a7596711533210c13d24c01a0147c60633
yourMD12 <yourmd12@gmail.com>
2021-09-30 10:47:53 -0400

    kva

--- InterviewCodingTasks/Array_FirstUniqueElement.java
+++ InterviewCodingTasks/Array_FirstUniqueElement.java
@@ -4,7 +4,7 @@ public class Array_FirstUniqueElement {
     write a program that can find the first duplicated element from the array
      */
 
-    public int firstDuplicatedElement(int[] array) {
+    public static int firstDuplicatedElement(int[] array) {
         int firstDuplicated = 0;
 
         for (int each : array) {
@@ -13,16 +13,16 @@ public class Array_FirstUniqueElement {
                 if (each == each2)
                     frequency++;
             }
-
             if (frequency > 1) {
                 firstDuplicated = each;
                 break;
             }
-
         }
-
         return firstDuplicated;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(firstDuplicatedElement(new int[]{2, 3, 4, 4, 5, 7, 8, 9, 2, 3}));
     }
 
 
