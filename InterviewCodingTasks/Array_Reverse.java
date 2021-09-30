030ea94631c511806e1bc27bb1ce85b05cec25b3
yourMD12 <yourmd12@gmail.com>
2021-09-30 10:48:29 -0400

    kva

--- InterviewCodingTasks/Array_Reverse.java
+++ InterviewCodingTasks/Array_Reverse.java
@@ -6,19 +6,17 @@ public class Array_Reverse {
     write a function that can reverse an array
      */
 
+    public static int[] reverse(int[] arr){  // [2,5,7,9,0,3,1]
 
-    public static int[] reverse(int[] arr){
-
-        int[] result = new int[arr.length];
-
+        int[] result = new int[arr.length]; // [0 0 0 0 0 0 0]
         for(int i = arr.length-1, j=0; i >= 0 ; i--, j++){
             result[j] = arr[i];
         }
-
         return result;
     }
 
-
-
+    public static void main (String [] args){
+        System.out.println(Arrays.toString(reverse(new int[]{2, 5, 7, 9, 0, 3, 1})));
+    }
 
 }
