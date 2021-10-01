e388d8c112285a009678dc837e71e0744e285cdb
yourMD12 <yourmd12@gmail.com>
2021-10-01 10:11:34 -0400

    kva

--- InterviewCodingTasks/List_sortingInAscOrDescOrder.java
+++ InterviewCodingTasks/List_sortingInAscOrDescOrder.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class List_sortingInAscOrDescOrder {
@@ -5,7 +7,7 @@ public class List_sortingInAscOrDescOrder {
     // Write a method that can sort the ArrayList in Ascending/descending order without using the sort method
 
     //asc
-    public static void SortingArrayListAsc(List<Integer> list) {
+    public static List<Integer> SortingArrayListAsc(List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.size(); j++) {
                 if (list.get(i) < list.get(j)) {
@@ -15,6 +17,7 @@ public class List_sortingInAscOrDescOrder {
                 }
             }
         }
+        return list;
     }
 
     //desc
@@ -28,6 +31,11 @@ public class List_sortingInAscOrDescOrder {
                 }
             }
         }
+
     }
 
+    public static void main(String[] args) {
+        SortingArrayListAsc(Arrays.asList(2,5,8,1,9,5,2,6));
+
+    }
 }
