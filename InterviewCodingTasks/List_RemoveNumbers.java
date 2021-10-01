791d4b32a8504673a18390aa860dc9f1e852dd37
yourMD12 <yourmd12@gmail.com>
2021-10-01 10:12:22 -0400

    kva

--- InterviewCodingTasks/List_RemoveNumbers.java
+++ InterviewCodingTasks/List_RemoveNumbers.java
@@ -9,12 +9,10 @@ public class List_RemoveNumbers {
     Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
      */
 
-
     // solution 1
     public List<Integer> remove1(List<Integer> list){
 
         Iterator<Integer> it = list.iterator();
-
         while (it.hasNext()){
             if(it.next()>100){
                 it.remove();
